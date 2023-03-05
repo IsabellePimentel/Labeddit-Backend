7e8c94a4b0796c62aaef66b16c969c5a98e8e744
isabellepimentel <isabellechavespimentel@gmail.com>
2023-03-05 20:18:49 -0300

    banco de dados- tabelas

--- src/database/labeddit.sql
+++ src/database/labeddit.sql
@@ -6,3 +6,91 @@ CREATE TABLE users(
     role TEXT NOT NULL,
     created_at TEXT DEFAULT (DATETIME()) NOT NULL
 );
+
+
+CREATE TABLE posts(
+    id TEXT PRIMARY KEY UNIQUE NOT NULL,
+    creator_id TEXT NOT NULL,
+    content TEXT NOT NULL,
+    likes INTEGER DEFAULT(0) NOT NULL,
+    dislikes INTEGER DEFAULT(0) NOT NULL,
+    comments INTEGER DEFAULT(0) NOT NULL,
+    created_at TEXT DEFAULT(DATETIME()) NOT NULL,
+    updated_at TEXT DEFAULT(DATETIME()) NOT NULL,
+    FOREIGN KEY (creator_id) REFERENCES users(id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
+);
+
+
+CREATE TABLE comments(
+    id PRIMARY KEY UNIQUE NOT NULL,
+    post_id UNIQUE NOT NULL,
+    user_id NOt NULL,
+    content TEXT NOT NULL,
+    likes INTEGER DEFAULT(0) NOT NULL,
+    dislikes INTEGER DEFAULT(0) NOT NULL,
+    created_at TEXT DEFAULT(DATETIME()) NOT NULL,
+    updated_at TEXT DEFAULT(DATETIME()) NOT NULL,
+    FOREIGN KEY (post_id) REFERENCES posts(id)
+        ON DELETE CASCADE 
+        ON UPDATE CASCADE,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
+);
+
+CREATE TABLE likes_dislikes_posts(
+    user_id TEXT NOT NULL,
+    post_id TEXT NOT NULL,
+    like INTEGER NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE,
+    FOREIGN KEY (post_id) REFERENCES posts(id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE 
+);
+
+
+CREATE TABLE likes_dislikes_comments(
+    user_id TEXT NOT NULL,
+    comment_id TEXT NOT NULL,
+    like INTEGER NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE,
+    FOREIGN KEY (comment_id) REFERENCES comments(id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE 
+);
+
+SELECT * FROM users;
+SELECT * FROM posts;
+SELECT * FROM comments;
+SELECT * FROM likes_dislikes_posts;
+SELECT * FROM likes_dislikes_comments;
+
+INSERT INTO users (id, name, email, password, role)
+VALUES
+    ("u001", "Isa", "isa@email.com", "IS@123", "ADMIN"),
+    ("u002", "Cris", "cris@email.com", "CRI$123", "NORMAL");
+
+INSERT INTO posts (id, creator_id, content)
+VALUES
+    ("p001", "u001", "Vocês terminaram o trabalho?!"),
+    ("p002", "u002", "Vamos ter aula hoje?!");
+
+INSERT INTO comments (id, post_id, user_id, content)
+VALUES
+    ("c001", "p001", "u002", "Eu terminei!"),
+    ("c002", "p002", "u001", "Vamos!");
+
+INSERT INTO likes_dislikes_posts (user_id, post_id, like)
+VALUES
+    ("u001", "p002", 1);
+
+
+INSERT INTO likes_dislikes_comments (user_id, comment_id, like)
+VALUES
+    ("u002", "c001", 1);
